590213bc4e2d5558f5b3508206284620bb782fec
Jonathan Krall <jonathan.krall@lexicon.se>
2025-11-10 11:45:31 +0100

    Added Person and its derived classes

---
+++ BasicOOP/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty; 
+    }
+    internal class Employee: Person
+    {
+        public int Salary { get; set; }
+    }
+    internal class Admin : Employee
+    {        
+        public string Department { get; set; } = string.Empty; 
+    }
+}
